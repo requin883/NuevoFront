2bec2d1d192d6fcb25c9bff202cbb6d528fa75d8
Victor Kneider <victorkneider@gmail.com>
2022-09-07 01:02:40 -0400

    feat: apply axios serverless to submit

--- src/Pages/Homepages/Components/Login.jsx
+++ src/Pages/Homepages/Components/Login.jsx
@@ -1,37 +1,60 @@
-import * as yup from "yup"
-import {useForm} from "react-hook-form";
+import * as yup from "yup";
+import { useForm } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
+import API_AXIOS from "../../../../settings/settings";
+import endpointList from "../../../../settings/endpoints";
+import { useNavigate } from "react-router-dom";
 
 const schema = yup.object().shape({
-    email: yup.string().email().required(),
-    password: yup.string().min(6).required()
+  email: yup.string().email().required(),
+  password: yup.string().min(6).required(),
 });
 
 function Login() {
-    const {register, formState:{errors}, handleSubmit} = useForm({
-        resolver: yupResolver(schema)
-      });
-    const fnSend = async(data) => {
-        alert(JSON.stringify(data))
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
+
+  const navigate = useNavigate();
+
+  const fnSend = async (data) => {
+    try {
+      let call = await API_AXIOS.get(
+        endpointList.login + `?email=${data.email}&password=${data.password}`
+      );
+      if (call.data == 0) {
+        alert("los datos no coinciden pana");
+      } else {
+        alert("tamo activo menol");
+        navigate('/menu')
       }
-    return (
-        <div>
-            <h1> Login </h1>
-            <form onSubmit={handleSubmit(fnSend)}> 
+    } catch (error) {
+      console.log(error);
+    }
+  };
+
+  return (
+    <div>
+      <h1> Login </h1>
+      <form onSubmit={handleSubmit(fnSend)}>
         <div>
           <label> Email </label>
-          <input type = "email" {...register("email")}/>
+          <input type="email" {...register("email")} />
           <p> {errors.email?.message}</p>
-        </div> 
+        </div>
         <div>
           <label> Password </label>
-          <input type="password" {...register("password")}/>
+          <input type="password" {...register("password")} />
           <p> {errors.password?.message}</p>
         </div>
-        <input type="submit" value="login"/>
-     </form>
-        </div>
-    )
+        <input type="submit" value="login" />
+      </form>
+    </div>
+  );
 }
 
 export default Login;
