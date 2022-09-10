4abb5ba5be3a4f7aae905c79a268aff9e38d77dd
requin883 <requin883@gmail.com>
2022-09-09 17:44:54 -0700

    feat: add chakraProvider and format

--- src/main.jsx
+++ src/main.jsx
@@ -1,20 +1,19 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { BrowserRouter as Browser, Route, Routes} from "react-router-dom"
+import { BrowserRouter as Browser, Route, Routes } from "react-router-dom"
 import Homepages from './Pages/Homepages/Homepages'
 import Menu from './Pages/Menu'
- 
-
-
+import { ChakraProvider } from '@chakra-ui/react'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Browser>
-    <Routes>
-      <Route path='/menu/*' element = {<Menu/>}/>
-      <Route path="/*" element = { <Homepages/>}/>
-    </Routes>
-    </Browser>
-   
+    <ChakraProvider>
+      <Browser>
+        <Routes>
+          <Route path='/menu/*' element={<Menu />} />
+          <Route path="/*" element={<Homepages />} />
+        </Routes>
+      </Browser>
+    </ChakraProvider>
   </React.StrictMode>
 )
