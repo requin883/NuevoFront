d61f1fa68a8b78774238b9ad36a0b40605875403
requin883 <requin883@gmail.com>
2022-09-09 17:45:26 -0700

    feat: add chakra button componet and colorScheme purple

--- src/callbacks/cbMenu.jsx
+++ src/callbacks/cbMenu.jsx
@@ -1,12 +1,8 @@
+import { Button } from "@chakra-ui/react";
 import { Link } from "react-router-dom";
 
-const cbMenu = (nm) => (
-   
-        <button className="button"> <Link key = {nm.options} to={nm.links}>  {nm.options} </Link> </button>
-
-        
-
-    
-)
+const cbMenu = (nm) => {
+        return (<Button key={nm.options} colorScheme="purple" size="lg" className="button"> <Link key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
+}
 
 export default cbMenu;
