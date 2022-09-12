8a2224168f7b44e7715c6c80cdfd79fd65f14ac1
requin883 <requin883@gmail.com>
2022-09-12 15:33:55 -0700

    feat: add token validation route for validating token

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter as Browser, Route, Routes } from "react-router-dom"
 import Homepages from './Pages/Homepages/Homepages'
 import Menu from './Pages/Menu'
 import { ChakraProvider } from '@chakra-ui/react'
+import Validation from './Pages/Homepages/Components/ValidateAcc';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
@@ -11,6 +12,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
       <Browser>
         <Routes>
           <Route path='/menu/*' element={<Menu />} />
+          <Route path="/validate/:token" element={<Validation/>}/>
           <Route path="/*" element={<Homepages />} />
         </Routes>
       </Browser>
