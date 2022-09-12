99b7e6fee88d202f007dd22fa6fafa9393325c04
requin883 <requin883@gmail.com>
2022-09-12 15:34:56 -0700

    feat: add stiling and modal to validatePayment

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,24 +1,22 @@
 import ValidatePays from "./ValidatePays/ValidatePays"
 import { Routes, Route } from "react-router-dom";
 import cbMenu from "../callbacks/cbMenu";
+import { Flex, Center, useDisclosure } from "@chakra-ui/react";
 
 function Menu() {
-
+    const { isOpen, onOpen, onClose } = useDisclosure();
     let menu = [
         { links: '/menu/validatepay', options: 'Validate Payments' },
-   
+
     ]
 
     return (
-        <div>
-    <div>
-        {menu.map(cbMenu)}
-    </div>
-            Menu
-            <Routes>
-                    <Route path='/validatepay' element={<ValidatePays/>} />
-             </Routes>
-        </div>
+        <Flex justify="center" width="100vw" h="100vh" opacity=".9" bgImage="url(../../Public/img/cryptoMenubg1.jpg)" bgSize="cover">
+            <Center onClick={onOpen}>
+                {menu.map(cbMenu)}
+            </Center>
+            <ValidatePays val={{ isOpen, onClose }}/>
+        </Flex>
     )
 }
 
