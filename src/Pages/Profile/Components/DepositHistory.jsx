f0e516c1be24345f997288d3dce8db71975c8c0a
requin883 <requin883@gmail.com>
2022-09-14 07:21:52 -0700

    feat: add style and open/close abilities

--- src/Pages/Profile/Components/DepositHistory.jsx
+++ src/Pages/Profile/Components/DepositHistory.jsx
@@ -1,8 +1,41 @@
+import { Box,
+    Table,
+    Thead,
+    Tbody,
+    Tfoot,
+    Tr,
+    Th,
+    Td,
+    TableCaption,
+    TableContainer,
+    Center, } from '@chakra-ui/react';
+
 function DepositHistory(){
     return (
-        <div> 
-            Deposits
-        </div> 
+        <Box>
+            <Center fontWeight="extrabold" fontSize="4xl" pt="1em" pb="1em">Deposits</Center>
+            <Center>
+            <TableContainer bgColor="purple.400" w="50em" rounded="10">
+                <Table variant="striped" >
+                    <Thead>
+                        <Tr>
+                            <Th> Quantity</Th>
+                            <Th> from/to </Th>
+                        </Tr>
+                    </Thead>
+                    <Tbody>
+                            {/* {transactions &&
+                                transactions.map((transaction) => (
+                                    <Tr>
+                                        <Td>{transaction.quantity}</Td>
+                                        <Td>{transaction.other}</Td>
+                                    </Tr>
+                                ))} */}
+                    </Tbody>
+                </Table>
+            </TableContainer>
+            </Center>
+        </Box>
     )
 }
 
