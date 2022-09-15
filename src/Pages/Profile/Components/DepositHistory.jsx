f1a4b7dd57fe988715b33c7fb448dd1e6b8a5b26
requin883 <requin883@gmail.com>
2022-09-14 17:53:07 -0700

    feat: add jesus functionality to deposits

--- src/Pages/Profile/Components/DepositHistory.jsx
+++ src/Pages/Profile/Components/DepositHistory.jsx
@@ -1,4 +1,5 @@
-import { Box,
+import {
+    Box,
     Table,
     Thead,
     Tbody,
@@ -8,32 +9,49 @@ import { Box,
     Td,
     TableCaption,
     TableContainer,
-    Center, } from '@chakra-ui/react';
+    Center,
+} from '@chakra-ui/react';
 
-function DepositHistory(){
+function DepositHistory() {
+
+    let [deposits, setDeposits] = useState([])
+    let [email, setEmail] = useState(window.localStorage.getItem("userEmailHP"))
+
+    const getData = async () => {
+        try {
+            let string = "?email=" + email.slice(1, email.length - 1)
+            console.log(string)
+            let { data } = await API_AXIOS.get(endpointList.getDeposits + string)
+            setDeposits(data)
+        } catch (error) {
+            console.log(error)
+        }
+
+    }
+
     return (
         <Box>
             <Center fontWeight="extrabold" fontSize="4xl" pt="1em" pb="1em">Deposits</Center>
             <Center>
-            <TableContainer bgColor="purple.400" w="50em" rounded="10">
-                <Table variant="striped" >
-                    <Thead>
-                        <Tr>
-                            <Th> Quantity</Th>
-                            <Th> from/to </Th>
-                        </Tr>
-                    </Thead>
-                    <Tbody>
-                            {/* {transactions &&
-                                transactions.map((transaction) => (
+                <TableContainer bgColor="purple.400" w="50em" rounded="10">
+                    <Table variant="striped" >
+                        <Thead>
+                            <Tr>
+                                <Th> Amount</Th>
+                                <Th> Currency </Th>
+                            </Tr>
+                        </Thead>
+                        <Tbody>
+                            {deposits &&
+                                deposits.map((deposits) => (
                                     <Tr>
-                                        <Td>{transaction.quantity}</Td>
-                                        <Td>{transaction.other}</Td>
+                                        <Td>{deposits.amount}</Td>
+                                        <Td>{deposits.currency}</Td>
                                     </Tr>
-                                ))} */}
-                    </Tbody>
-                </Table>
-            </TableContainer>
+                                ))}
+                        </Tbody>
+                    </Table>
+                </TableContainer>
             </Center>
         </Box>
     )
