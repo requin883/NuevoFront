732a2e0eb586888345c27adf0d039691b007f98a
requin883 <requin883@gmail.com>
2022-09-14 17:49:05 -0700

    feat: add background component

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,32 +1,37 @@
 import ValidatePays from "./ValidatePays/ValidatePays"
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import cbMenu from "../callbacks/cbMenu";
-import { Flex, Center, useDisclosure } from "@chakra-ui/react";
+import { Flex, Button, useDisclosure, useBoolean } from "@chakra-ui/react";
 import Profile from "./Profile/Profile";
 import Transactions from "./Transactions/Transactions";
+import Background from "./Background/Background";
 
 function Menu() {
     const { isOpen, onOpen, onClose } = useDisclosure();
+
+    const [flag, setFlag] = useBoolean();
+
     let menu = [
-        { links: '/menu/validatepay', options: 'Validate Payments' },
-        { links: '/menu/transactions', options: 'Make a transaction'},
-        { links: '/menu/profile', options: 'Profile' }
+        { links: '/menu/validatepay', options: 'Validate Payments', opt: 'vp' },
+        { links: '/menu/transactions', options: 'Make a transaction', opt: 'mt' },
+        { links: '/profile', options: 'Profile', opt: 'pf' },
     ]
 
-    return (
-        <Flex justify="center" width="100vw" h="100vh" opacity=".9" bgImage="url(../../Public/img/cryptoMenubg1.jpg)" bgSize="cover">
-            <Center onClick={onOpen}>
-                {menu.map(cbMenu)}
-            </Center>
-            <ValidatePays val={{ isOpen, onClose }} />
-            <Routes>
-                <Route path='/validatepay' element={<ValidatePays />} />
-                <Route path="/profile/*" element={<Profile />} />
-                <Route path="/transactions" element = {<Transactions/>} />
-                <Route path='/validatepay' element={<ValidatePays/>} />
-            </Routes>
-        </Flex>
+    const cbMenu = (nm) => {
+        return (<Button key={nm.options} onClick={nm.opt == 'vp' ? onOpen : nm.opt == 'mt' ? setFlag.on : () => 0} colorScheme="purple" size="lg" className="button"> <Link key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
+    }
 
+    return (
+        <Background position="absolute">
+            <Flex h="5em" bgColor="black" align="center" justify="center" gap="1em">
+                {menu.map(cbMenu)}
+            </Flex>
+            <Routes>
+                <Route path='/validatepay' element={<ValidatePays val={{ isOpen, onClose }} />} />
+                <Route path="/transactions" element={<Transactions val={{ flag, setFlag }} />} />
+            </Routes>
+        </Background>
     )
 }
+
 export default Menu
