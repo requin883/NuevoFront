d371a2b9eecf70f6443c88b484a2120f6c374bab
requin883 <requin883@gmail.com>
2022-09-19 00:56:15 -0700

    feat: change to bootstrap functionality

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,17 +1,19 @@
 import ValidatePays from "./ValidatePays/ValidatePays"
 import { Routes, Route, Link } from "react-router-dom";
-import cbMenu from "../callbacks/cbMenu";
-import { Flex, Button, useDisclosure, useBoolean } from "@chakra-ui/react";
-import Profile from "./Profile/Profile";
+import { Container, Button } from "reactstrap";
 import Transactions from "./Transactions/Transactions";
 import { useLocalStorage } from "../hooks/useLocalStorage";
 import { useEffect } from "react";
 import ProtectedRoute from "../ProtectedRoute";
+import ExamplesNavbar from "./Homepages/Components/Navbar";
+import { useState } from "react";
 
 function Menu() {
-    const { isOpen, onOpen, onClose } = useDisclosure();
-    let [userLogin, setUserLogin] = useLocalStorage('user', "") 
-    const [flag, setFlag] = useBoolean();
+    const [valFlag, setValFlag] = useState(false);
+    const [transFlag, setTransFlag] = useState(false);
+    const [profileFlag, setProfileFlag] = useState(false);
+
+    let [userLogin, setUserLogin] = useLocalStorage('user', "")
 
     let menu = [
         { links: '/menu/validatepay', options: 'Validate Payments', opt: 'vp' },
@@ -20,28 +22,29 @@ function Menu() {
     ]
 
     const cbMenu = (nm) => {
-        return (<Button key={nm.options} onClick={nm.opt == 'vp' ? onOpen : nm.opt == 'mt' ? setFlag.on : () => 0} colorScheme="purple" size="lg" className="button"> <Link key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
+        return (<Button key={nm.options} onClick={nm.opt == 'vp' ? () => setValFlag(true) : nm.opt == 'mt' ? () => setTransFlag(true) : () => setProfileFlag(true)}  size="lg" > <Link style={{color:"white",textDecoration:"none"}} key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
     }
-    useEffect (() => {
+    useEffect(() => {
         let date = new Date()
-              setUserLogin(date)
+        setUserLogin(date)
     }, [])
     return (
-        <Background position="absolute">
-            <Flex h="5em" bgColor="black" align="center" justify="center" gap="1em">
+        <>
+            <ExamplesNavbar />
+            <Container className="linkscontainer d-flex justify-content-evenly">
                 {menu.map(cbMenu)}
-            </Flex>
+            </Container>
             <Routes>
-            <Route element={<ProtectedRoute/>}>
-                <Route path='/validatepay' element={<ValidatePays val={{ isOpen, onClose }} />} />
-            </Route>
-   
-            <Route element={<ProtectedRoute/>}>
-                <Route path="/transactions" element={<Transactions val={{ flag, setFlag }} />} />
-            </Route>
-   
+                <Route element={<ProtectedRoute />}>
+                    <Route path='/validatepay' element={<ValidatePays val={{ valFlag, setValFlag }} />} />
+                </Route>
+
+                <Route element={<ProtectedRoute />}>
+                    <Route path="/transactions" element={<Transactions val={{ transFlag, setTransFlag }} />} />
+                </Route>
+
             </Routes>
-        </Background>
+        </>
     )
 }
 
