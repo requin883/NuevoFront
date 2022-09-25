59a40ae05124cfddf908b288d0db5eabbca7552d
requin883 <requin883@gmail.com>
2022-09-25 08:09:45 -0700

    feat: add images to the cryptocurrencies

--- src/Pages/Homepages/Components/Card.jsx
+++ src/Pages/Homepages/Components/Card.jsx
@@ -1,14 +1,20 @@
 import { Col, Button } from "reactstrap"
 
+
+
+
 export default function Card({ title, cat, children }) {
+
+    const route = "../../../../Public/assets";
+    const imgSrc = title=="USDT"?`${route}/tether.png`:title=="BTC"?`${route}/btc.png`:title=="SOL"?`${route}/Solana.png`:`${route}/binance.png`;
     return (
         <>
-            <Col md="4" key={title} className="card me-3 mb-3">
+            <Col md="3" key={title} className="card me-3 mb-3">
                 <div className="d-flex flex-column alert newcard">
-                    <h4 className="align-self-center">{title}</h4>
+                    <h4 className="align-self-center">{title}<img className="ps-2" style={{maxWidth:"3vw"}} src={imgSrc} ></img></h4>
                     <p className="category text-info">{cat}</p>
                     <p>
-                    {children}
+                        {children}
                     </p>
                     <Button
                         className="btn-icon btn-round"
