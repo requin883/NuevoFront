427579c7cd6247d1315b070977d3da5beb72a257
requin883 <requin883@gmail.com>
2022-09-25 11:20:58 -0700

    feat: add functionality to EmailAlert and spinner functionality

--- src/Pages/Homepages/Components/EmailAlert.jsx
+++ src/Pages/Homepages/Components/EmailAlert.jsx
@@ -8,8 +8,11 @@ import {
     Input,
     Label,
     FormGroup,
+    Container,
+    ModalFooter,
+    Spinner,
 } from "reactstrap";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { forgetPwSchema } from '../../../Utils/yupSchemas';
 import API_AXIOS from '../../../settings/settings';
@@ -20,10 +23,11 @@ import { useState } from 'react';
 function EmailAlert(props) {
     const [spinner, setSpinner] = useState(false);
 
-    const { flag, setFlag } = props.val;
+    const { valFlag, setValFlag } = props.val;
 
     const {
         register,
+        control,
         formState: { errors },
         handleSubmit,
         reset,
@@ -32,12 +36,14 @@ function EmailAlert(props) {
     });
 
 
+    const { ref, ...emailField } = register("email");
+
     const fnSend = async (data) => {
         setSpinner(true);
         let { email } = data;
         let registeredFlag = await API_AXIOS.get(`${endpointList.findEmail}?email=${email}`);
         if (registeredFlag.data) {
-            await API_AXIOS.post(`${endpointList.forgetPw}?email=${email}`)
+            await API_AXIOS.post(`${endpointList.forgotPassword}?email=${email}`)
             alert("Recibirá un correo para cambiar su contraseña");
         } else {
             alert("El correo no está registrado");
@@ -48,20 +54,40 @@ function EmailAlert(props) {
     }
 
     return (
-        <Modal isOpen={flag} size="md" className="text-dark">
-            <ModalHeader className="clearfix text-center text-black">
-                Forgot Password
-            </ModalHeader>
+        <Modal isOpen={valFlag} size="md" className="text-dark">
+            <Container className="text-black">
+                <ModalHeader>
+                    Forgot Password
+                </ModalHeader>
+            </Container>
             <ModalBody>
                 <Form className="text-center" onSubmit={handleSubmit(fnSend)}>
-                    <FormGroup floating>
-                        <Input name="email" id="email" placeholder="Email" type="email" {...register("email")} />
-                        <Label for="email">Email</Label>
-                        <FormFeedback>{errors.email?.message}</FormFeedback>
-                    </FormGroup>
-                    {spinner ? <Button mt="2em" disabled={spinner} colorScheme="purple" type="submit"><Spinner /></Button> : <Button mt="2em" colorScheme="purple" type="submit">Enviar</Button>}
+                    <Controller
+                        control={control}
+                        name="email"
+                        render={({ field: { ref, ...emailField } }) => (
+                            <FormGroup floating>
+                                <Input
+                                    bsSize="sm"
+                                    name="email"
+                                    placeholder="email"
+                                    type="email"
+
+                                    invalid={errors.email ? true : false}
+                                    innerRef={ref} {...emailField}
+                                />
+
+                                <Label for="email"> Email </Label>
+                                {errors?.email && (
+                                    <FormFeedback>{errors.email?.message}</FormFeedback>
+                                )}
+                            </FormGroup>
+                        )}
+                    />
+                    {spinner ? <Button mt="2em" disabled={spinner} className="btn-menu" color="info" type="submit"><Spinner /></Button> : <Button mt="2em" className="btn-menu text-light" color="info" type="submit">Enviar</Button>}
                 </Form>
             </ModalBody>
+            <ModalFooter><Button disabled={spinner} className="btn-menu text-light" color="info" >X</Button></ModalFooter>
         </Modal>
     )
 }
