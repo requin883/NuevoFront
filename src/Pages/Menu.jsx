e5cbf3a258c818d054189ead43fca34fa53a86c8
requin883 <requin883@gmail.com>
2022-09-25 09:23:03 -0700

    feat: add button functionality to avoid issues with Link

--- src/Pages/Menu.jsx
+++ src/Pages/Menu.jsx
@@ -1,5 +1,5 @@
 import ValidatePays from "./ValidatePays/ValidatePays"
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Container, Button } from "reactstrap";
 import Transactions from "./Transactions/Transactions";
 import { useLocalStorage } from "../hooks/useLocalStorage";
@@ -15,22 +15,30 @@ function Menu() {
     const [transFlag, setTransFlag] = useState(false);
     const [profileFlag, setProfileFlag] = useState(false);
 
+    const navigate = useNavigate();
+
     let [userLogin, setUserLogin] = useLocalStorage('user', "")
 
     let menu = [
         { links: '/menu/validatepay', options: 'Validate Payments', opt: 'vp' },
         { links: '/sendpayment', options: 'Make a transaction', opt: 'mt' },
         { links: '/profile', options: 'Profile', opt: 'pf' },
-        {links:'/', options:'Return to main menu'}
+        { links: '/home', options: 'Return to main menu' }
     ]
 
     const cbMenu = (nm) => {
-        return (<Button key={nm.options} className="btn-menu" color="info" onClick={nm.opt == 'vp' ? () => setValFlag(true) : nm.opt == 'mt' ? () => setTransFlag(true) : () => setProfileFlag(true)}  size="lg" > <Link style={{color:"white",textDecoration:"none"}} key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
+        const handleClick = () => {
+            nm.opt == 'vp' ? setValFlag(true) :
+                nm.opt == 'mt' ? setTransFlag(true) :
+                    setProfileFlag(true);
+            navigate(nm.links);
+        }
+        return (<Button key={nm.options} className="btn-menu m-3 p-3 text-light" color="info" onClick={handleClick} size="lg" >{nm.options}</Button>)
     }
     useEffect(() => {
         let date = new Date()
         setUserLogin(date)
-    }, [])
+    }, []);
     return (
         <>
             <ExamplesNavbar />
@@ -42,7 +50,7 @@ function Menu() {
                 <Route element={<ProtectedRoute />}>
                     <Route path='/validatepay' element={<ValidatePays val={{ valFlag, setValFlag }} />} />
                 </Route>
-                    <Route path="/transactions" element={<Transactions val={{ transFlag, setTransFlag }} />} />
+                <Route path="/transactions" element={<Transactions val={{ transFlag, setTransFlag }} />} />
             </Routes>
         </>
     )
