97df14d2441031b1f4bf38020347ef91f6433746
requin883 <requin883@gmail.com>
2022-09-25 11:22:08 -0700

    hotfix: change Links with usenavigate to prevent button issues add pro variable for navbar

--- src/Pages/Profile/Profile.jsx
+++ src/Pages/Profile/Profile.jsx
@@ -1,5 +1,5 @@
 import { useEffect, useState } from "react";
-import { Routes, Route, Link } from "react-router-dom";
+import { Routes, Route, useNavigate } from "react-router-dom";
 import { Card, Container, Button, CardTitle, CardBody } from "reactstrap";
 import endpointList from "../../../settings/endpoints";
 import API_AXIOS from "../../../settings/settings";
@@ -16,6 +16,8 @@ export default function Profile() {
     const [depositFlag, setDepositFlag] = useState(false);
     const [balanceFlag, setBalanceFlag] = useState(false);
 
+    const navigate = useNavigate();
+
     let [email, setEmail] = (useState(window.localStorage.getItem("userEmailHP")))
 
     // let [userLogin, setUserLogin] = useLocalStorage('user', "")
@@ -25,7 +27,13 @@ export default function Profile() {
     // historial de depositos
 
     const cbMenu = (nm) => {
-        return (<Button color="info" className="btn-menu mb-4" key={nm.options} onClick={()=>nm.opt == "ph" ? setPaymentFlag(true) : nm.opt == "dh" ? setDepositFlag(true) : nm.opt == "pr" ? setBalanceFlag(true) : ""} size="lg"> <Link style={{ color: "white", textDecoration: 'none' }} key={nm.options} to={nm.links}>  {nm.options} </Link> </Button>)
+        const handleClick = () => {
+            nm.opt == 'ph' ? setPaymentFlag(true) :
+                nm.opt == 'dh' ? setDepositFlag(true) :
+                    nm.opt == 'pr' ? setBalanceFlag(true) : "";
+            navigate(nm.links)
+        }
+        return (<Button color="info" className="btn-menu mb-4 p-3 text-light" key={nm.options} onClick={handleClick} size="lg">{nm.options}</Button>)
     }
 
     // useEffect(() => {
@@ -47,7 +55,7 @@ export default function Profile() {
 
     return (
         <>
-            <ExamplesNavbar />
+            <ExamplesNavbar env="pro" />
             <Container className="d-flex  profmar flex-column align-items-center justify-content-center">
                 <Container className="mt-5">
                     <h1 className="text-center text-decoration-underline mb-4">Menu</h1>
@@ -57,15 +65,15 @@ export default function Profile() {
                     </Container>
                     <Container>
                         <Routes>
-                             {/* <Route element={<ProtectedRoute />}>  */}
-                            <Route path='/paymenthistory' element={<PaymentHistory val={{ paymentFlag, setPaymentFlag }} />} />
-                            {/* </Route>  */}
-                             <Route element={<ProtectedRoute />}> 
-                            <Route path="/deposithistory" element={<DepositHistory val={{ depositFlag, setDepositFlag }} />} />
-                             </Route> 
-                             <Route element={<ProtectedRoute />}>
-                            <Route path="/balance" element={<Balance val={{ balanceFlag, setBalanceFlag }} />} />
+                            <Route element={<ProtectedRoute />}> 
+                            <Route path='/paymenthistory' element={<PaymentHistory val={{ paymentFlag, setPaymentFlag }} from="profile" />} />
                             </Route> 
+                            <Route element={<ProtectedRoute />}>
+                                <Route path="/deposithistory" element={<DepositHistory val={{ depositFlag, setDepositFlag }} />} />
+                            </Route>
+                            <Route element={<ProtectedRoute />}>
+                                <Route path="/balance" element={<Balance val={{ balanceFlag, setBalanceFlag }} />} />
+                            </Route>
                         </Routes>
                     </Container>
                 </Container>
