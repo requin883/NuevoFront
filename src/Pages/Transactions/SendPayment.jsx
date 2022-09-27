0a3eec6f88e4d205ee6d6ac8f525f5b848b0d859
requin883 <requin883@gmail.com>
2022-09-26 20:26:29 -0700

    feat: take email from local storage implement a function to retrieve info and show alerts and add modal to send authorization code

--- src/Pages/Transactions/SendPayment.jsx
+++ src/Pages/Transactions/SendPayment.jsx
@@ -5,7 +5,7 @@ import { Route, Routes, useNavigate } from "react-router-dom";
 import { sendPaymentSchema } from "../../Utils/yupSchemas";
 import API_AXIOS from "../../../settings/settings";
 import endpointList from "../../settings/endpoints";
-import { Container, Form, Label, Input, FormGroup, FormFeedback, Button, Spinner } from "reactstrap";
+import { Container, Form, Label, Input, FormGroup, FormFeedback, Button, Spinner, Modal, ModalHeader, ModalBody, ModalFooter, Alert } from "reactstrap";
 import ExamplesNavbar from "../Homepages/Components/Navbar";
 import ProtectedRoute from "../../ProtectedRoute";
 import PaymentHistory from "../Profile/Components/PaymentHistory";
@@ -24,10 +24,33 @@ function SendPayment() {
 
     const navigate = useNavigate();
 
+    let [email, setEmail] = useState(window.localStorage.getItem("userEmailHP"));
+
+    const [valCode, setValCode] = useState(false);
+
+    const [modalAlert, setModalAlert] = useState(false);
+
+    const [msg, setMsg] = useState("The number can't be less or equal than 0");
+
+    const [newAlert, setNewAlert] = useState(false);
+
     const [spinner, setSpinner] = useState(false);
 
     const [paymentFlag, setPaymentFlag] = useState(false);
     const [balanceFlag, setBalanceFlag] = useState(false);
+
+    const handleAlert = () => {
+        setNewAlert(true);
+        setTimeout(() => {
+            setNewAlert(false)
+        }, 3000);
+    }
+    const handleModalAlert = () => {
+        setModalAlert(true);
+        setTimeout(() => {
+            setModalAlert(false)
+        }, 3000);
+    }
 
     let [switchbtn, setSwitchBtn] = useState(true);
 
@@ -35,20 +58,48 @@ function SendPayment() {
 
     const currencies = ["usdt", "btc", "eth", "busd"];
 
-    const fnSend = (data) => {
+    const fnSend = async (data) => {
         setSpinner(true);
+        let sender = email;
+        if (Number(data.amount) <= 0) {
+            handleAlert();
+            setSpinner(false);
+            return;
+        }
         if (!switchbtn) {
             let token = data.data;
             let currency = data.currency;
             let amount = data.amount;
-            alert(`Token=${token} & currency=${currency} & amount ${amount}`)
+            // const info = await API_AXIOS.post(`${endpointList.sendPayment}?sender=${sender}&receiver=${token}&quantity=${amount}&token=${currency}`)
         } else {
             let email = data.data;
             let currency = data.currency;
             let amount = data.amount;
-            alert(`Email=${email} & currency=${currency} & amount ${amount}`)
+            console.log("hello");
+            const info = await API_AXIOS.post(`${endpointList.sendPayment}?sender=${sender.slice(1, -1)}&receiver=${email}&quantity=${amount}&token=${currency}`);
+            console.log(info.data);
+            switch (info.data) {
+                case 0:
+                    setMsg("The receiver user doesn't exist");
+                    handleAlert();
+                    break;
+                case 1:
+                    setMsg("You don't have enought balance to process this transaction");
+                    handleAlert();
+                    break;
+                case 2:
+                    setMsg("You can't send money to your own account");
+                    handleAlert();
+                    break;
+                case 3:
+                case 4:
+                    setValCode(true);
+                    break;
+                default:
+                    break;
+            }
         }
-           setSpinner(false);
+        setSpinner(false);
     }
 
 
@@ -60,6 +111,18 @@ function SendPayment() {
         } else {
             setPlaceholder("Add receiver email");
         }
+    }
+
+    const handleVerCodeSubmit = async ({ verToken }) => {
+        if (!Number.isInteger(Number(verToken))) {
+            setMsg("The verification code must contain only numbers");
+            handleModalAlert();
+            return;
+        } else {
+            const info = await API_AXIOS.get(`${endpointList.verifyVerCode}?email=${email.slice(1, -1)}&code=${verToken}`)
+            console.log(info.data);
+        }
+
     }
 
     const navBal = () => {
@@ -83,114 +146,156 @@ function SendPayment() {
     return (
         <>
             <ExamplesNavbar env="pro" />
-            <Container className="cont d-flex justify-content-between">
-                <Container>
-                    <Container className="d-flex flex-column gap-3">
-                        <Container className="bg-light text-dark rounded">
-                            <h1 className="text-center pt-3">Payment Types</h1>
-                            <ul>
-                                <li>Public: Used to send payments by email address</li>
-                                <li className="pb-3">Private: Used to send payments with secret token </li>
-                            </ul>
-                        </Container>
-                        <Button disabled={spinner} color="info" onClick={navBal} className="p-3 btn-menu text-light">Show Balance</Button>
-                        <Button disabled={spinner} color="info" onClick={navPayment} className="p-3 btn-menu text-light">Show Payment History</Button>
-                        <Button disabled={spinner} color="info" onClick={handleExport} className="p-3 btn-menu text-light">Show Export payments to Excel</Button>
-                        <Button disabled={spinner} color="info" onClick={menuNavigate} className="p-3 btn-menu text-light">Return to Menu</Button>
-                    </Container>
-                </Container>
-                <div className="separator"></div>
-                <Container className="d-flex flex-column align-items-center">
-                    <h1 className="text-center"> Send Payment </h1>
-                    <FormGroup switch className="ms-auto">
-                        <Input type="switch" onChange={toggleBtn} role="switch" />
-                        <Label check>{switchbtn ? "Public" : "Secret"}</Label>
-                    </FormGroup>
-                    <Form onSubmit={handleSubmit(fnSend)}>
+            <Modal isOpen={valCode} style={{ marginTop: "20vh" }} className="text-dark">
+                <Alert isOpen={modalAlert} className="m-2 text-center" color="danger">
+                    {msg}
+                </Alert>
+                <ModalHeader>Verification Code</ModalHeader>
+                <ModalBody>
+                    <h6 className="text-center pb-4">Please type the authorization code that has been sent to you email for verification purposes</h6>
+                    <Form onSubmit={handleSubmit(handleVerCodeSubmit)}>
                         <Controller
                             control={control}
-                            name="data"
-                            render={({ field: { ref, ...dataProps } }) => (
-                                <FormGroup>
-                                    <Label for="data"> {placeholder} </Label>
+                            name="verToken"
+                            render={({ field: { ref, ...verTokenProps } }) => (
+                                <FormGroup floating>
                                     <Input
-                                        className="controlledInput"
-                                        name="data"
-                                        placeholder={placeholder}
+                                        name="verToken"
+                                        placeholder="Authorization Code"
                                         type="text"
-                                        id="data"
-
-                                        invalid={errors.data ? true : false}
-                                        innerRef={ref} {...dataProps}
+                                        id="verToken"
+                                        invalid={errors.verToken ? true : false}
+                                        innerRef={ref} {...verTokenProps}
                                     />
-                                    {errors?.data && (
-                                        <FormFeedback>{errors.data?.message}</FormFeedback>
+                                    <Label for="verToken">Enter verification Code</Label>
+                                    {errors?.verToken && (
+                                        <FormFeedback>{errors.verToken?.message}</FormFeedback>
                                     )}
                                 </FormGroup>
                             )}
                         />
-                        <Controller
-                            control={control}
-                            name="amount"
-                            render={({ field: { ref, ...amountProps } }) => (
-                                <FormGroup>
-                                    <Label for="amount"> Amount  </Label>
-                                    <Input
-
-                                        name="amount"
-                                        placeholder="Put amount"
-                                        type="number"
-                                        id="amount"
-
-                                        invalid={errors.amount ? true : false}
-                                        innerRef={ref} {...amountProps}
-                                    />
-
-
-                                    {errors?.amount && (
-                                        <FormFeedback>{errors.amount?.message}</FormFeedback>
-                                    )}
-                                </FormGroup>
-                            )}
-                        />
-
-                        <Controller
-                            defaultValue="usdt"
-                            control={control}
-                            name="currency"
-                            render={({ field: { ref, ...currencyProps } }) => (
-                                <FormGroup>
-                                    <Label for="Currency"> Currency </Label>
-                                    <Input
-
-                                        name="currency"
-                                        placeholder="Select currency"
-                                        type='select'
-                                        id="currency"
-
-                                        invalid={errors.currency ? true : false}
-                                        innerRef={ref} {...currencyProps}
-                                    >
-                                        <optgroup label="Choose a currency">
-                                            {currencies?.map((option, index) => (
-                                                <option key={index} value={option}>
-                                                    {option}
-                                                </option>
-                                            ))}
-                                        </optgroup>
-                                    </Input>
-
-
-                                    {errors?.currency && (
-                                        <FormFeedback>{errors.currency?.message}</FormFeedback>
-                                    )}
-                                </FormGroup>
-                            )}
-                        />
-                        <Container className="text-center">
-                            {spinner ? <Button color="info" disabled={spinner} className="btn-menu text-light" type="submit"><Spinner /></Button> : <Button color="info" className="btn-menu text-light" type="submit">Send Payment</Button>}
+                        <Container className="d-flex justify-content-center">
+                            <Button className="btn-menu" color="info">Verify Code</Button>
                         </Container>
                     </Form>
+                </ModalBody>
+                <ModalFooter></ModalFooter>
+            </Modal>
+
+            <Container className="cont flex-column d-flex justify-content-between">
+                <Alert isOpen={newAlert} className="m-2 text-center" color="danger">
+                    {msg}
+                </Alert>
+                <Container className="d-flex justify-content-between" style={{ marginTop: "5em" }}>
+                    <Container >
+                        <Container className="d-flex flex-column gap-3">
+                            <Container className="bg-light text-dark rounded">
+                                <h1 className="text-center pt-3">Payment Types</h1>
+                                <ul>
+                                    <li>Public: Used to send payments by email address</li>
+                                    <li className="pb-3">Private: Used to send payments with secret token </li>
+                                </ul>
+                            </Container>
+                            <Button disabled={spinner} color="info" onClick={navBal} className="p-3 btn-menu text-light">Show Balance</Button>
+                            <Button disabled={spinner} color="info" onClick={navPayment} className="p-3 btn-menu text-light">Show Payment History</Button>
+                            <Button disabled={spinner} color="info" onClick={handleExport} className="p-3 btn-menu text-light">Show Export payments to Excel</Button>
+                            <Button disabled={spinner} color="info" onClick={menuNavigate} className="p-3 btn-menu text-light">Return to Menu</Button>
+                        </Container>
+                    </Container>
+                    <div className="separator"></div>
+                    <Container className="d-flex flex-column align-items-center">
+                        <h1 className="text-center"> Send Payment </h1>
+                        <FormGroup switch className="ms-auto">
+                            <Input type="switch" disabled={spinner} onChange={toggleBtn} role="switch" />
+                            <Label check>{switchbtn ? "Public" : "Secret"}</Label>
+                        </FormGroup>
+                        <Form onSubmit={handleSubmit(fnSend)}>
+                            <Controller
+                                control={control}
+                                name="data"
+                                render={({ field: { ref, ...dataProps } }) => (
+                                    <FormGroup>
+                                        <Label for="data"> {placeholder} </Label>
+                                        <Input
+                                            disabled={spinner}
+                                            className="controlledInput"
+                                            name="data"
+                                            placeholder={placeholder}
+                                            type="text"
+                                            id="data"
+
+                                            invalid={errors.data ? true : false}
+                                            innerRef={ref} {...dataProps}
+                                        />
+                                        {errors?.data && (
+                                            <FormFeedback>{errors.data?.message}</FormFeedback>
+                                        )}
+                                    </FormGroup>
+                                )}
+                            />
+                            <Controller
+                                control={control}
+                                name="amount"
+                                render={({ field: { ref, ...amountProps } }) => (
+                                    <FormGroup>
+                                        <Label for="amount"> Amount  </Label>
+                                        <Input
+                                            disabled={spinner}
+                                            name="amount"
+                                            placeholder="Put amount"
+                                            type="text"
+                                            id="amount"
+
+                                            invalid={errors.amount ? true : false}
+                                            innerRef={ref} {...amountProps}
+                                        />
+
+
+                                        {errors?.amount && (
+                                            <FormFeedback>{errors.amount?.message}</FormFeedback>
+                                        )}
+                                    </FormGroup>
+                                )}
+                            />
+
+                            <Controller
+                                defaultValue="usdt"
+                                control={control}
+                                name="currency"
+                                render={({ field: { ref, ...currencyProps } }) => (
+                                    <FormGroup>
+                                        <Label for="Currency"> Currency </Label>
+                                        <Input
+
+                                            name="currency"
+                                            placeholder="Select currency"
+                                            type='select'
+                                            id="currency"
+                                            disabled={spinner}
+                                            invalid={errors.currency ? true : false}
+                                            innerRef={ref} {...currencyProps}
+                                        >
+                                            <optgroup label="Choose a currency">
+                                                {currencies?.map((option, index) => (
+                                                    <option key={index} value={option}>
+                                                        {option}
+                                                    </option>
+                                                ))}
+                                            </optgroup>
+                                        </Input>
+
+
+                                        {errors?.currency && (
+                                            <FormFeedback>{errors.currency?.message}</FormFeedback>
+                                        )}
+                                    </FormGroup>
+                                )}
+                            />
+                            <Container className="text-center">
+                                {spinner ? <Button color="info" disabled={spinner} className="btn-menu text-light" type="submit"><Spinner /></Button> : <Button color="info" className="btn-menu text-light" type="submit">Send Payment</Button>}
+                            </Container>
+                        </Form>
+                    </Container>
                 </Container>
             </Container>
             <Routes>
