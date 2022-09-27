86cccdd18d0465c2be2e11ea5b4a096082d50149
requin883 <requin883@gmail.com>
2022-09-26 20:28:11 -0700

    feat: change from Modal to Card and fix some typos and remove flags for modal

--- src/Pages/ValidatePays/ValidatePays.jsx
+++ src/Pages/ValidatePays/ValidatePays.jsx
@@ -2,9 +2,11 @@ import { useForm, Controller } from "react-hook-form";
 import { yupResolver } from "@hookform/resolvers/yup";
 import { validatePaySchema } from "../../Utils/yupSchemas";
 import {
-  Modal,
-  ModalHeader,
-  ModalBody,
+  Card,
+  CardTitle,
+  CardHeader,
+  CardBody,
+  CardFooter,
   Form,
   FormGroup,
   Label,
@@ -13,21 +15,25 @@ import {
   Button,
   Spinner,
   Container,
-  ModalFooter,
 } from 'reactstrap'
 import { useEffect, useState } from "react";
 import API_AXIOS from "../../../settings/settings";
 import endpointList from "../../../settings/endpoints";
 import { useLocalStorage } from "../../hooks/useLocalStorage";
+import ExamplesNavbar from "../Homepages/Components/Navbar";
+import { useNavigate } from "react-router-dom";
 
 
 
-function ValidatePays(props) {
-  let [userEmail, setEmail] = useState(window.localStorage.getItem("userEmailHP"));
-  let [userLogin, setUserLogin] = useLocalStorage('user', "")
+function ValidatePays() {
+
+  const navigate = useNavigate();
+
+  const [userEmail, setEmail] = useState(window.localStorage.getItem("userEmailHP"));
+
+  const [userLogin, setUserLogin] = useLocalStorage('user', "");
+
   const [spinner, setSpinner] = useState(false);
-
-  let { valFlag, setValFlag } = props.val;
 
   const {
     register,
@@ -46,6 +52,7 @@ function ValidatePays(props) {
       let output = await API_AXIOS.post(endpointList.verifyPayment + string)
       alert(JSON.stringify(output.data))
       setSpinner(false);
+      reset();
     } catch (error) {
       console.log(error)
     }
@@ -55,176 +62,180 @@ function ValidatePays(props) {
     setUserLogin(date)
   }, [])
   return (
-    <Modal className="text-dark" isOpen={valFlag} size="lg">
-      <ModalHeader className="d-flex justify-content-evenly">
-        Validate payment
-      </ModalHeader>
+    <Container>
+      <ExamplesNavbar env="pro" />
+      <Card className="text-dark" style={{marginTop:"20vh"}}>
+        <CardHeader >
+          <CardTitle className="text-center fw-bold">
+            Validate payment
+          </CardTitle>
+        </CardHeader>
+        <CardBody>
+          <Form onSubmit={handleSubmit(fnSend)}>
+            <Controller
+              control={control}
+              name="year"
+              render={({ field: { ref, ...yearProps } }) => (
+                <FormGroup>
+                  <Label for="year"> Year </Label>
+                  <Input
 
-      <ModalBody>
-        <Form onSubmit={handleSubmit(fnSend)}>
-          <Controller
-            control={control}
-            name="year"
-            render={({ field: { ref, ...yearProps } }) => (
-              <FormGroup>
-                <Label for="year"> Year </Label>
-                <Input
+                    name="year"
+                    placeholder="Year"
+                    type="number"
+                    id="year"
 
-                  name="year"
-                  placeholder="Year"
-                  type="number"
-                  id="year"
+                    invalid={errors.year ? true : false}
+                    innerRef={ref} {...yearProps}
+                  />
+                  {errors?.year && (
+                    <FormFeedback>{errors.year?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="month"
+              render={({ field: { ref, ...monthProps } }) => (
+                <FormGroup>
+                  <Label for="month"> Month </Label>
+                  <Input
 
-                  invalid={errors.year ? true : false}
-                  innerRef={ref} {...yearProps}
-                />
-                {errors?.year && (
-                  <FormFeedback>{errors.year?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="month"
-            render={({ field: { ref, ...monthProps } }) => (
-              <FormGroup>
-                <Label for="month"> Month </Label>
-                <Input
+                    name="month"
+                    placeholder="Month"
+                    type="number"
+                    id="month"
 
-                  name="month"
-                  placeholder="Month"
-                  type="number"
-                  id="month"
+                    invalid={errors.month ? true : false}
+                    innerRef={ref} {...monthProps}
+                  />
+                  {errors?.month && (
+                    <FormFeedback>{errors.month?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="day"
+              render={({ field: { ref, ...dayProps } }) => (
+                <FormGroup>
+                  <Label for="day"> Day </Label>
+                  <Input
 
-                  invalid={errors.month ? true : false}
-                  innerRef={ref} {...monthProps}
-                />
-                {errors?.month && (
-                  <FormFeedback>{errors.month?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="day"
-            render={({ field: { ref, ...dayProps } }) => (
-              <FormGroup>
-                <Label for="day"> Day </Label>
-                <Input
+                    name="day"
+                    placeholder="Day"
+                    type="number"
+                    id="day"
 
-                  name="day"
-                  placeholder="Day"
-                  type="number"
-                  id="day"
+                    invalid={errors.day ? true : false}
+                    innerRef={ref} {...dayProps}
+                  />
+                  {errors?.day && (
+                    <FormFeedback>{errors.day?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="hour"
+              render={({ field: { ref, ...hourProps } }) => (
+                <FormGroup>
+                  <Label for="hour"> Hour </Label>
+                  <Input
 
-                  invalid={errors.day ? true : false}
-                  innerRef={ref} {...dayProps}
-                />
-                {errors?.day && (
-                  <FormFeedback>{errors.day?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="hour"
-            render={({ field: { ref, ...hourProps } }) => (
-              <FormGroup>
-                <Label for="hour"> Hour </Label>
-                <Input
+                    name="hour"
+                    placeholder="Hour"
+                    type="number"
+                    id="hour"
 
-                  name="hour"
-                  placeholder="Hour"
-                  type="number"
-                  id="hour"
+                    invalid={errors.hour ? true : false}
+                    innerRef={ref} {...hourProps}
+                  />
+                  {errors?.hour && (
+                    <FormFeedback>{errors.hour?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="minute"
+              render={({ field: { ref, ...minuteProps } }) => (
+                <FormGroup>
+                  <Label for="minute"> Minutes </Label>
+                  <Input
 
-                  invalid={errors.hour ? true : false}
-                  innerRef={ref} {...hourProps}
-                />
-                {errors?.hour && (
-                  <FormFeedback>{errors.hour?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="minute"
-            render={({ field: { ref, ...minuteProps } }) => (
-              <FormGroup>
-                <Label for="minute"> Minutes </Label>
-                <Input
+                    name="minute"
+                    placeholder="Minutes"
+                    type="number"
+                    id="minute"
 
-                  name="minute"
-                  placeholder="Minutes"
-                  type="number"
-                  id="minute"
+                    invalid={errors.minute ? true : false}
+                    innerRef={ref} {...minuteProps}
+                  />
+                  {errors?.minute && (
+                    <FormFeedback>{errors.minute?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="second"
+              render={({ field: { ref, ...secondProps } }) => (
+                <FormGroup>
+                  <Label for="second"> Seconds </Label>
+                  <Input
 
-                  invalid={errors.minute ? true : false}
-                  innerRef={ref} {...minuteProps}
-                />
-                {errors?.minute && (
-                  <FormFeedback>{errors.minute?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="second"
-            render={({ field: { ref, ...secondProps } }) => (
-              <FormGroup>
-                <Label for="second"> Seconds </Label>
-                <Input
+                    name="second"
+                    placeholder="Seconds"
+                    type="number"
+                    id="second"
 
-                  name="second"
-                  placeholder="Seconds"
-                  type="number"
-                  id="second"
+                    invalid={errors.second ? true : false}
+                    innerRef={ref} {...secondProps}
+                  />
+                  {errors?.second && (
+                    <FormFeedback>{errors.second?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Controller
+              control={control}
+              name="amount"
+              render={({ field: { ref, ...amountProps } }) => (
+                <FormGroup>
+                  <Label for="amount"> Amount </Label>
+                  <Input
 
-                  invalid={errors.second ? true : false}
-                  innerRef={ref} {...secondProps}
-                />
-                {errors?.second && (
-                  <FormFeedback>{errors.second?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Controller
-            control={control}
-            name="amount"
-            render={({ field: { ref, ...amountProps } }) => (
-              <FormGroup>
-                <Label for="amount"> Amount </Label>
-                <Input
+                    name="amount"
+                    placeholder="Amount"
+                    type="number"
+                    id="amount"
 
-                  name="amount"
-                  placeholder="Amount"
-                  type="number"
-                  id="amount"
-
-                  invalid={errors.amount ? true : false}
-                  innerRef={ref} {...amountProps}
-                />
-                {errors?.amount && (
-                  <FormFeedback>{errors.amount?.message}</FormFeedback>
-                )}
-              </FormGroup>
-            )}
-          />
-          <Container className="text-center">
-            {spinner ? <Button disabled={spinner} type="submit" className="btn-menu text-light" color="info" value="register"><Spinner /></Button> : <Button type="submit" className="text-center btn-menu text-light" color="info" value="register">Validate</Button>}
-          </Container>
-        </Form>
-      </ModalBody>
-      <ModalFooter>
-        <Button disabled={spinner} className="btn-menu text-light" color="info" onClick={() => setValFlag(false)}>X</Button>
-      </ModalFooter>
-    </Modal>
+                    invalid={errors.amount ? true : false}
+                    innerRef={ref} {...amountProps}
+                  />
+                  {errors?.amount && (
+                    <FormFeedback>{errors.amount?.message}</FormFeedback>
+                  )}
+                </FormGroup>
+              )}
+            />
+            <Container className="text-center">
+              {spinner ? <Button disabled={spinner} type="submit" className="btn-menu text-light" color="info" value="register"><Spinner /></Button> : <Button type="submit" className="text-center btn-menu text-light" color="info" value="register">Validate</Button>}
+            </Container>
+          </Form>
+        </CardBody>
+        <CardFooter className="text-center">
+          <Button disabled={spinner} className="btn-menu text-light" color="info" onClick={() => navigate("/menu")}>Go back to main menu</Button>
+        </CardFooter>
+      </Card>
+    </Container>
   )
 
 }
