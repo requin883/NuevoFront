01ca62dae165316011ac203ca5d133eda9d3fa09
requin883 <requin883@gmail.com>
2022-09-26 20:23:28 -0700

    feat: add style to component and modify inputs

--- src/Pages/ForgotPassword/ForgotPassword.jsx
+++ src/Pages/ForgotPassword/ForgotPassword.jsx
@@ -1,15 +1,19 @@
 
 import API_AXIOS from "../../../settings/settings";
-import * as yup from "yup" 
+import * as yup from "yup"
 import { useParams } from "react-router-dom";
 import { yupResolver } from "@hookform/resolvers/yup";
-import { useForm } from "react-hook-form";
+import { useForm, Controller } from "react-hook-form";
 import endpointList from "../../settings/endpoints";
+import { Card, CardBody, CardFooter, Alert, CardHeader, CardTitle, Container, Form, FormGroup, Label, Input, Button } from "reactstrap";
+import ExamplesNavbar from "../Homepages/Components/Navbar";
+import { useState } from "react";
 
 
 const redirectUser = () => {
     window.location.href = "http://localhost:5173"
 }
+
 
 const schema = yup.object().shape({
     password: yup.string().min(6).required(),
@@ -19,46 +23,99 @@ const schema = yup.object().shape({
         .oneOf([yup.ref("password")], "the password confirmation does not match"),
 })
 
-function ForgotPassword (){
+function ForgotPassword() {
+
     const {
         register,
+        control,
         formState: { errors },
         handleSubmit,
-      } = useForm({
+    } = useForm({
         resolver: yupResolver(schema),
-      });
+    });
 
+    const [newAlert, setNewAlert] = useState(false);
 
-const params = useParams()
-let email = params.email
+    const params = useParams();
+    let email = params.email
 
-const fnSend = async (data) => {
-    let output = await API_AXIOS.post(endpointList.forgotPassword + `?email=${email}&newPassword=${data.password}`)
-    if (output.data == 1) {
-        alert ("Contraseña cambiada correctamente")
+    const fnSend = async (data) => {
+        let output = await API_AXIOS.post(endpointList.forgotPassword + `?email=${email}&newPassword=${data.password}`)
+        if (output.data == 1) {
+            alert("Contraseña cambiada correctamente")
+        }
     }
-}
 
-return(
-    <div> 
-        <h2> Email: {email} </h2> 
-        <h1> Save a new password </h1>
-        <form onSubmit={handleSubmit(fnSend)}>
-            <div>
-                 <label> Password </label>
-                 <input type="password" placeholder="Password" {...register("password")} />
-                 <p> {errors.password?.message} </p>
-            </div>
-            <div>
-                 <label> Verify new Password </label>
-                 <input type="password" placeholder="Same password " {...register("valpass")} />
-                 <p> {errors.valpass?.message} </p>
-            </div>
-          <input type="submit" value="Save new password" />
-        </form>
-        <button onClick={redirectUser}> Go to home page </button>
-    </div>
-)
+    return (
+        <Container>
+            <ExamplesNavbar />
+            <Container className="text-dark" style={{ marginTop: "30vh" }}>
+                <Card>
+                    <CardHeader className="text-center" >
+                    <Alert isOpen color="danger">
+                        The token has expired. Please go back to login page to request a new link sent out to your email address
+                    </Alert>
+                        <CardTitle className="p-4" style={{ fontWeight: "bold" }}>Update Password</CardTitle>
+                    </CardHeader>
+                    <CardBody>
+
+                        <Form onSubmit={handleSubmit(fnSend)}>
+                            <Controller
+                                control={control}
+                                name="password"
+                                render={({ field: { ref, ...passProps } }) => (
+                                    <FormGroup floating>
+                                        <Input
+
+                                            name="password"
+                                            placeholder="Password"
+                                            type="password"
+                                            id="password"
+
+                                            invalid={errors.password ? true : false}
+                                            innerRef={ref} {...passProps}
+                                        />
+                                        <Label for="password">Password</Label>
+
+                                        {errors?.password && (
+                                            <FormFeedback>{errors.password?.message}</FormFeedback>
+                                        )}
+                                    </FormGroup>
+                                )}
+                            />
+
+                            <Controller
+                                control={control}
+                                name="valpass"
+                                render={({ field: { ref, ...valpassProps } }) => (
+                                    <FormGroup floating>
+                                        <Input
+
+                                            name="valpass"
+                                            placeholder="Confirm password"
+                                            type="password"
+                                            id="valpass"
+
+                                            invalid={errors.valpass ? true : false}
+                                            innerRef={ref} {...valpassProps}
+                                        />
+                                        <Label for="valpass"> Confirm Password </Label>
+
+                                        {errors?.valpass && (
+                                            <FormFeedback>{errors.valpass?.message}</FormFeedback>
+                                        )}
+                                    </FormGroup>
+                                )}
+                            />
+                        </Form>
+                    </CardBody>
+                    <CardFooter className="text-center">
+                        <Button className="btn-menu" color="info" onClick={redirectUser}> Update now!</Button>
+                    </CardFooter>
+                </Card>
+            </Container>
+        </Container>
+    )
 
 }
 
